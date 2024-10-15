86d99c124a94ed53a0f18bc313f6398607d036da
Andreas Chioccchetti <46024652+achiocch@users.noreply.github.com>
2024-10-15 10:12:09 +0000

    update req

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ import plotly.io as pio
 # Initialize the Dash app
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
+server = app.server
 
 # Define the layout of the app
 app.layout = dbc.Container([
